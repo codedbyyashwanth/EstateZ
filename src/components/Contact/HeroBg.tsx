89c2f63fce76eefadf6c7e62139449d6e690d943
codedbyyashwanth <yashwanthpcmc5@gmail.com>
2024-01-18 18:00:44 +0530

    Contact Page is Completed

---
+++ src/components/Contact/HeroBg.tsx
@@ -0,0 +1,18 @@
+
+
+const HeroBg = () => {
+      return (
+            <div className="property_hero">
+                  <div className="container">
+                        <div className="heading_container">
+                              <div className="left_container">
+                                    <h1 className="h2">Get in Touch with Estatein</h1>
+                                    <p className="text">Welcome to Estatein's Contact Us page. We're here to assist you with any inquiries, requests, or feedback you may have. Whether you're looking to buy or sell a property, explore investment opportunities, or simply want to connect, we're just a message away. Reach out to us, and let's start a conversation. </p>
+                              </div>
+                        </div>
+                  </div>
+            </div>
+      )
+}
+
+export default HeroBg;
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,6 +1,12 @@
 import { FC } from "react";
 import Navbar from "../components/Navbar";
 import Banner from "../components/Banner";
+import HeroBg from "../components/Contact/HeroBg";
+import Features from "../components/Home/Features";
+import ReachOut from "../components/Properties/ReachOut";
+import Clients from "../components/About/Clients";
+import CalltoAction from "../components/CalltoAction";
+import Footer from "../components/Footer";
 
 
 const Contact:FC = () => {
@@ -8,9 +14,15 @@ const Contact:FC = () => {
             <header>
                   <Banner />
                   <Navbar isActive="contact" />
+                  <HeroBg />
             </header>
-            <main className="contact-main"></main>
-            <footer></footer>
+            <main className="contact_main">
+                  <Features />
+                  <ReachOut />
+                  <Clients />
+                  <CalltoAction />
+            </main>
+            <Footer />
       </>
 }
 
